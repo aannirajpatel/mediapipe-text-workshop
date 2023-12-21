b98c85fdfb748396cf5b627c3d32f2dc7144c939
Aan Niraj Patel <patelaan13@gmail.com>
2023-12-20 22:18:58 -0500

    chore: initial commit - scaffold app using create-react-app, add TypeScript, MUI

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <Button variant="contained">Hello World</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App.tsx';
+import reportWebVitals from './reportWebVitals';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootElement
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
